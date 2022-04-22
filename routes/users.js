b667f1103c1b570f1c5362939dfd89a523aea15e
sri <sriyarocking98@gmail.com>
2022-04-22 15:26:33 +0530

    adding code for mongo

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var mongoURL="mongodb://admin:India2020@172.30.75.153:27017";
+const dbname = "sampledb";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/db', function(req, res, next) {
+  mongoClient.connect(mongoURL,(err,res)=>{
+    if(err){
+      console.log("error connecting db");
+    }
+    else{
+      const db = res.db(dbname);
+      db.collection("catalog").find({}).toArray((err,res)=>{
+        if(err){
+          console.log('err in collection')
+        }
+        else{
+          console.log(res)
+        }
+      });
+      res.close();
+    }
+  })
+ });
 module.exports = router;
